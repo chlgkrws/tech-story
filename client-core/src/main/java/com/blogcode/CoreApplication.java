ff15e6035c2f3afae31a5994bf1ce5225cd6e801
zeeeeeno <2zeeno@naver.com>
2021-07-07 17:00:08 +0900

    Main 화면 구성

---
+++ client-core/src/main/java/com/blogcode/CoreApplication.java
@@ -0,0 +1,22 @@
+package com.blogcode;
+
+/**
+ * <pre>
+ * com.blogcode
+ *
+ * Description :
+ * </pre>
+ *
+ * @author leejinho
+ * @version 1.0
+ * @see <pre>
+ * == 개정이력(Modification Information) ==
+ *
+ * 수정일     수정자   수정내용
+ * ---------- -------- -------------------
+ * 2021.07.07 leejinho 최초 생성
+ * </pre>
+ * @since 2021.07.07
+ */
+public class CoreApplication {
+}
