4b0e5425cef226e6fff4fcc212d00c5c7bb21bfa
it1457 <cgw981@naver.com>
2021-07-10 02:39:13 +0900

    blog-domain 의존성 제거

--- blog-domain/src/main/java/com/blogcode/BlogApplication.java
+++ blog-domain/src/main/java/com/blogcode/BlogApplication.java
@@ -1,8 +1,7 @@
-package com;
+package com.blogcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 /**
  * <pre>
--- blog-domain/src/main/java/com/repository/PostsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.repository;
-
-import com.domain.Posts;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostsRepository extends JpaRepository<Posts, Long> {
-}
