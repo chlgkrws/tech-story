03d9a5159d7c6df0b332b540b5e27b1e181e9299
chlgkrws <61148082+chlgkrws@users.noreply.github.com>
2021-07-27 13:41:59 +0900

    AOP Setting (Front)

--- kvelog-app-front/src/main/java/com/blogcode/web/MainController.java
+++ kvelog-app-front/src/main/java/com/blogcode/web/MainController.java
@@ -2,6 +2,7 @@ package com.blogcode.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * <pre>
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @since 2021-06-26
  */
 @Controller
+@RequestMapping("/")
 public class MainController {
     @GetMapping
     public String main() {
