0469e86b408cf3c17e6d88f2da69f3fafbe89984
chlgkrws <61148082+chlgkrws@users.noreply.github.com>
2021-07-27 13:42:15 +0900

    AOP Setting (internal-api)

--- kvelog-app-internal-api/src/test/java/com/blogcode/api/BlogRestControllerTest.java
+++ kvelog-app-internal-api/src/test/java/com/blogcode/api/BlogRestControllerTest.java
@@ -17,11 +17,16 @@ import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.headers.HeaderDocumentation;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
+import static org.springframework.restdocs.headers.HeaderDocumentation.*;
 import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
 import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -55,12 +60,13 @@ public class BlogRestControllerTest {
         Posts posts = Posts.builder()
                 .title("블로그 글 생성")
                 .content("테스트")
-                .writerEmail("cgw981@naver.com")
+                .writerEmail(member.getEmail())
                 .writerName("최학준")
                 .thumbnailPath("/qwdqw/dwqdwq/dwqdwq")
                 .dType(PostType.BLOG)
                 .member(member)
                 .build();
+
 
             // Then
             this.mockMvc.perform(post("/api/blog")
@@ -77,14 +83,43 @@ public class BlogRestControllerTest {
                     .andExpect(jsonPath("member.id").exists())
                     .andExpect(jsonPath("member.email").exists())
                     .andDo(document("create-blog",
-                        links(
-                                linkWithRel("self").description("생성된 Blog 조회"),
-                                linkWithRel("query-blogs").description("Blog 목록 조회"),
-                                linkWithRel("update-blog").description("생성된 Blog 수정"),
-                                linkWithRel("profile").description("해당 Rest API profile 이동")
-                        )
+                            links(
+                                    linkWithRel("self").description("생성된 Blog 조회"),
+                                    linkWithRel("query-blogs").description("Blog 목록 조회"),
+                                    linkWithRel("update-blog").description("생성된 Blog 수정"),
+                                    linkWithRel("profile").description("해당 Rest API profile 이동")
+                            ),
+                            requestHeaders(
+                                    headerWithName(HttpHeaders.ACCEPT).description("accept 헤더"),
+                                    headerWithName(HttpHeaders.CONTENT_TYPE).description("content type 헤더")
+                            ),
+                            relaxedRequestFields(
+                                    fieldWithPath("title").description("생성할 Blog의 제목"),
+                                    fieldWithPath("content").description("생성할 Blog 내용"),
+                                    fieldWithPath("writerEmail").description("생성할 Blog 작성자 이메일"),
+                                    fieldWithPath("writerName").description("생성할 Blog의 작성자 이름"),
+                                    fieldWithPath("tempSaveStatus").description("생성할 Blog의 임시저장 여부"),
+                                    fieldWithPath("dtype").description("생성할 Blog의 타입(블로그/Q&A)")
+                            ),
+                            responseHeaders(
+                                    headerWithName(HttpHeaders.CONTENT_TYPE).description("Content type - Hal Json"),
+                                    headerWithName(HttpHeaders.LOCATION).description("응답 Location")
+                            ),
+                            relaxedResponseFields(
+                                    fieldWithPath("id").description("생성된 Blog의 식별자"),
+                                    fieldWithPath("title").description("생성된 Blog의 제목"),
+                                    fieldWithPath("content").description("생성된 Blog의 내용"),
+                                    fieldWithPath("writerEmail").description("생성된 Blog 작성자 이메일"),
+                                    fieldWithPath("writerName").description("생성된 Blog의 작성자 이름"),
+                                    fieldWithPath("thumbnailPath").description("생성된 Blog의 썸네일 경로"),
+                                    fieldWithPath("tempSaveStatus").description("생성된 Blog의 임시저장여부"),
+                                    fieldWithPath("member.id").description("생성된 Blog의 작성자 식별자"),
+                                    fieldWithPath("dtype").description("생성된 Blog의 타입(블로그/Q&A)")
+                            )
 
-                        ))
+
+
+                    ))
         ;
 
     }
@@ -96,6 +131,9 @@ public class BlogRestControllerTest {
                 .name("최학준")
                 .password("hi")
                 .build();
+        member.setCreateDateTime(LocalDateTime.of(2022,11,11,10,10,10));
+        member = this.memberRepository.save(member);
+        member.setCreateDateTime(LocalDateTime.of(2022,11,11,10,10,10));
         member = this.memberRepository.save(member);
         System.out.println(member.getId());
         return member;
