d85029819fb8defef7fd8dcb2bc1d6128ec988d9
it1457 <cgw981@naver.com>
2021-08-12 22:41:09 +0900

    oauth-server 테스트 코드 작성

--- oauth-app/src/main/java/com/blogcode/api/Test.java
+++ oauth-app/src/main/java/com/blogcode/api/Test.java
@@ -1,16 +1,30 @@
 package com.blogcode.api;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+
 @RestController
-@RequestMapping("/")
+@RequestMapping("/bearer")
 public class Test {
 
-    @GetMapping
+    @GetMapping("/root")
     public String testController(){
+        System.out.println("@@@@@@@@@@@@");
         return "hi";
     }
 
+    @GetMapping("test")
+    public String testController2(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        System.out.println("authorities = " + authorities);
+        System.out.println("!!!!!!!!!!");
+        return "hi";
+    }
 }
--- oauth-app/src/test/java/com/blogcode/config/OAuthTokenTest.java
+++ oauth-app/src/test/java/com/blogcode/config/OAuthTokenTest.java
@@ -2,16 +2,24 @@ package com.blogcode.config;
 
 import com.blogcode.member.domain.Member;
 import com.blogcode.member.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.common.util.Jackson2JsonParser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.Map;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -43,19 +51,73 @@ public class OAuthTokenTest {
 
         String clientId = "kvelog";
         String clientSecret = "kvsecret";
-        this.mockMvc.perform(post("/oauth/token")
+        ResultActions resultActions = this.mockMvc.perform(post("/oauth/token")
                 .with(httpBasic(clientId, clientSecret))
                 .param("username", username)
                 .param("password", password)
-                .param("grant_type","password")
+                .param("grant_type", "password")
         )
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("access_token").exists())
-                .andDo(print())
-        ;
+                .andDo(print());
 
+        MockHttpServletResponse response = resultActions.andReturn().getResponse();
+        String contentAsString = response.getContentAsString();
+        Jackson2JsonParser jackson2JsonParser = new Jackson2JsonParser();
+        Map<String, Object> stringObjectMap = jackson2JsonParser.parseMap(contentAsString);
+        String access_token = stringObjectMap.get("access_token").toString();
+        System.out.println(access_token);
+    }
+
+    @Test
+    @DisplayName("토큰 포함 / 요청 보내기")
+    public void requestWithToken() throws Exception {
+
+        // Given
+        String token = "3a6c9a7e-8d9c-4f62-aa6e-742cfd8a0730";
+
+        ResultActions resultActions = this.mockMvc.perform(get("/bearer/test")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + getToken2())
+        )
+                .andDo(print());
+
+        String contentAsString = resultActions.andReturn().getResponse().getContentAsString();
+        Assertions.assertThat(contentAsString).isEqualTo("hi");
 
     }
+
+
+
+    public String getToken2() throws Exception {
+        // Given
+        String username = "cg32421421432w981@naver.com31";
+        String password = "pass";
+        Member hakjun  = Member.builder()
+                .email(username)
+                .password(passwordEncoder.encode(password))
+                .build();
+        this.memberRepository.save(hakjun);
+
+        String clientId = "kvelog";
+        String clientSecret = "kvsecret";
+        ResultActions resultActions = this.mockMvc.perform(post("/oauth/token")
+                .with(httpBasic(clientId, clientSecret))
+                .param("username", username)
+                .param("password", password)
+                .param("grant_type", "password")
+        )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("access_token").exists())
+                .andDo(print());
+
+        MockHttpServletResponse response = resultActions.andReturn().getResponse();
+        String contentAsString = response.getContentAsString();
+        Jackson2JsonParser jackson2JsonParser = new Jackson2JsonParser();
+        Map<String, Object> stringObjectMap = jackson2JsonParser.parseMap(contentAsString);
+        String access_token = stringObjectMap.get("access_token").toString();
+        return access_token;
+    }
+
 
 
 
