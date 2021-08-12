0b88c280a2c406a448861b0876b45f6939a005ee
zeeeeeno <2zeeno@naver.com>
2021-08-12 14:03:01 +0900

    MapStruct 추가

---
+++ domain-rds/src/main/java/com/blogcode/mapper/BlogMapper.java
@@ -0,0 +1,23 @@
+package com.blogcode.mapper;
+
+import com.blogcode.posts.domain.Posts;
+import com.blogcode.posts.dto.BlogDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+/**
+ * Description : blotDto <-> Posts
+ *
+ * @author leejinho
+ * @version 1.0
+ */
+@Mapper
+public interface BlogMapper extends StructMapper<BlogDto, Posts> {
+    BlogMapper INSTANCE = Mappers.getMapper(BlogMapper.class);
+
+//    IF not use extends, add this code
+//    BlogDto toDto(Posts posts);
+//    Posts toEntity(BlogDto blogDto);
+//    List<BlogDto> toDtoList(List<Posts> posts);
+//    List<Posts> toEntityList(List<BlogDto> blogDtos);
+}
---
+++ domain-rds/src/main/java/com/blogcode/mapper/StructMapper.java
@@ -0,0 +1,19 @@
+package com.blogcode.mapper;
+
+import com.blogcode.posts.domain.Posts;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Description : mapstruct interface (Entity <-> Dto
+ *
+ * @author leejinho
+ * @version 1.0
+ */
+public interface StructMapper <Dto, Entity> {
+    Dto toDto(Optional<Posts> entity);
+    Entity toEntity(Dto dto);
+    List<Dto> toDtoList(List<Entity> entityList);
+    List<Entity> toEntityList(List<Dto> dtoList);
+}
